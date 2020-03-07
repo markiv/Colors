4aea532c7d13bd4a168613cde964f3f466ed1292
Vikram Kriplaney <markiv@gmail.com>
2020-03-07 21:20:00 +0100

    Initial Commit

---
+++ Colors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colors
+//
+//  Created by Vikram Kriplaney on 07.03.2020.
+//  Copyright © 2020 iPhonso GmbH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
